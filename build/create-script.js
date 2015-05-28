ac436c6a49ddf0b78a664e7b94faf8e86fab8429
davey <davey@dpwoert.com>
2015-05-28 21:42:11 +0200

    fix for building script

--- build/create-script.js
+++ build/create-script.js
@@ -1,17 +1,12 @@
 var IO = require('cityio');
-
-module.exports = function(){
-
-    var build = new IO.classes.Build();
-    build
-        .add('classes.Cycle')
-        .add('build.buildings')
-        .add('build.areas')
-        .add('build.roads')
-        .add('FXlib.tiltShift')
-        .export('cityio.js')
-        .then(function(){
-            console.log('done');
-        });
-
-}
+var build = new IO.classes.Build();
+build
+    .add('classes.Cycle')
+    .add('build.buildings')
+    .add('build.areas')
+    .add('build.roads')
+    .add('FXlib.tiltShift')
+    .export('cityio.js')
+    .then(function(){
+        console.log('updated cityio script');
+    });
